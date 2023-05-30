50411f294bbd2d6cb0ccf54c855b4117a7f6db92
quizitocristiano <quizitocristiano10@gmail.com>
2023-05-30 17:00:44 -0300

    nova

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./assets/pages/home";
 import { Login } from "./assets/componetelogin/login";
-import './App.css'
+import "./App.css";
 function App() {
-
-
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route path="/hyy" element={<Home />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/ddfhd" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  )
-};
+  );
+}
 
-export default App
+export default App;
